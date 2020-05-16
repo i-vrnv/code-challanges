ad72e3d36c0d7062cb67be7822de6003632d8b3d
Ignatiy Voronov <ignatiy.voronov@gmail.com>
2020-05-16 13:40:50 +0300

    121. Best Time to Buy and Sell Stock O(n) Kadane's algo

--- src/main/java/leetcode/Problem_121_Best_Time_to_Buy_and_Sell_Stock/Solution.java
+++ src/main/java/leetcode/Problem_121_Best_Time_to_Buy_and_Sell_Stock/Solution.java
@@ -1,25 +1,17 @@
 package leetcode.Problem_121_Best_Time_to_Buy_and_Sell_Stock;
 
 /**
- * Runtime: 247 ms, faster than 13.06% of Java online submissions for Best Time to Buy and Sell Stock.
- * Memory Usage: 39.5 MB, less than 20.35% of Java online submissions for Best Time to Buy and Sell Stock.
+* Runtime: 1 ms, faster than 99.17% of Java online submissions for Best Time to Buy and Sell Stock.
+* Memory Usage: 39.5 MB, less than 21.24% of Java online submissions for Best Time to Buy and Sell Stock.
  */
 class Solution {
     public int maxProfit(int[] prices) {
-        if (prices.length == 0) return 0;
-
-        int maxProfit = 0;
-
-        for (int i = 1; i < prices.length; i++) {
-            for (int j = i; j >= 0; --j) {
-                if (prices[i] > prices[j]) {
-                    int profit = prices[i] - prices[j];
-                    if (profit > maxProfit) {
-                        maxProfit = profit;
-                    }
-                }
-            }
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for (int c: prices) {
+            min = Math.min(c, min);
+            max = Math.max(max, c - min);
         }
-        return maxProfit;
+        return max;
     }
 }
